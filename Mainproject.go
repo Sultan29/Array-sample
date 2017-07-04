1032364bd836a02481a79ba268af780e22a79480
Sultan <sultanmaidanov@gmail.com>
2017-07-04 18:43:07 +0800

    Update Mainproject.go

--- Mainproject.go
+++ Mainproject.go
@@ -6,10 +6,8 @@ import (
 	"io"
 	"strconv"
 	"net/url"
-
 )
 var x[10]string
-
 
 func main(){
 
@@ -24,37 +22,20 @@ func main(){
 	x[8] = "Water"
 	x[9] = "Ice"
 	fmt.Println(x)
-
-
 	http.HandleFunc("/getall", getall)
 	http.HandleFunc("/get", get)
 	http.ListenAndServe(":8080", nil)
 }
-
-
 func getArray() (arrr []int){
 	mass := []int{11,20,40,25,24,17,24,35,40,25}
 	println(mass)
 	return mass
-
 }
-
-
-
 func getall(w http.ResponseWriter, r *http.Request){
-
-
-
 	for r,num := range getArray(){
-
-
-
 		io.WriteString(w,"<p> "+x[r]+" price "+strconv.Itoa(num)+"</p>")
 	}
-
 }
-
-
 func get(w http.ResponseWriter, r *http.Request) {
 
 
@@ -64,19 +45,13 @@ func get(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(attr)
 	id := attr["id"][0]
 	fmt.Println("id of request " + id)
-
-
-
 	v := id
 	d:= getArray()
 
 		if a, err := strconv.Atoi(v);
-
 			err == nil {
-
-			io.WriteString(w, "<p> "+x[a-1]+" price "+strconv.Itoa(d[a-1])+"</p>")
+				io.WriteString(w, "<p> "+x[a-1]+" price "+strconv.Itoa(d[a-1])+"</p>")
 		}
 
-
-	}
+}
 
